d020a8437ca61300bc386a84be5e5b4bff88bfc2
Yevheniia Horbachova <gor.jenia2003@gmail.com>
2023-03-13 22:10:39 -0600

    Completed findKey function

---
+++ findKey.js
@@ -0,0 +1,17 @@
+const findKey = function (object, callback) {
+    const keys = Object.keys(object);
+    for (const key of keys) {
+        if (callback(object[key])) {
+            return key;
+        }
+    } return undefined;
+}
+
+console.log(findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  }, x => x.stars === 2)) // => "noma"
